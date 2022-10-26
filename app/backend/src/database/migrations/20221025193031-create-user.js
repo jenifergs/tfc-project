9b108ed932597d49f284a4b9530c6cfdbee4d055
jenifergs <contato.comercialjenifer@gmail.com>
2022-10-26 10:38:54 -0300

    feat: :white_check_mark: Requirement 1

--- app/backend/src/database/migrations/20221025193031-create-user.js
+++ app/backend/src/database/migrations/20221025193031-create-user.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userName: {
+      username: {
         type: Sequelize.STRING
       },
       role: {
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
